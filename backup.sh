18cb15fac9d8c66565b3c75149078c4b837c959a
Pat Findley <pat@findley.cc>
2020-05-24 23:32:05 -0500

    Adding working backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,15 @@
-rsync -av -delete --exclude-from='/root/exclude-file.txt' / /media/1tb/rpi4
+#!/bin/bash
 
-zip --password PASSWORD /media/1tb/rpi4-$(date +"%Y-%m-%d-%h").zip /media/1tb/rpi4/
+#Created 23May2020,ptf
 
-find /var/log -name "*.zip" -type f -mtime +30 -exec rm -f {} \;
+rsync -av --delete /home/ /media/1tb/backups/home
+rsync -av --delete /etc/ /media/1tb/backups/etc
+rsync -av --delete --exclude '/root/exclude.txt' /var/ /media/1tb/backups/var
+rsync -av --delete /root/ /media/1tb/backups/root
 
-rm -rf /media/1tb/rpi4/*
+#cd /media/1tb/archive && zip -r "archive-$(date +"%Y-%m-%d").zip" /media/1tb/backups/ -x /media/1tb/backups/var/run/speech-dispatcher/\*
+#timestamp= $( date +%y-%m-%d-%H_%M)
+#name="(date +"%y-%m-%d")"
+cd /media/1tb/archive && tar -zcvf $( date '+%y-%m-%d-%H_%M' ).tar.gz /media/1tb/backups
+
+find /media/1tb/archive -name "*.tar.gz" -type f -mtime +30 -exec rm -f {} \;
