bc894358839e5fe7d2737c8803ce9e15b10f0482
pf1359 <pat@findleyfamilies.com>
2020-10-20 22:42:00 -0500

    fixed pfticker

--- pfticker.py
+++ pfticker.py
@@ -1,5 +1,13 @@
-import gnewsclient
 from gnewsclient import gnewsclient
-tclient = gnewsclient.NewsClient(language='english', location='United States', topic='Technology', max_results=5)
 
-tclient.get_config()
+client = gnewsclient.NewsClient(language='english',
+                                location='United States',
+                                topic='technology',
+                                max_results=3)
+
+news_list = client.get_news()
+
+for item in news_list:
+    print("Title : ", item['title'])
+    print("Link : ", item['link'])
+    print("")
