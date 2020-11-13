6ca25a7b8c4e1ac099e173b59a7bd99ee105a8aa
Pat Findley <pat@findley.cc>
2020-11-13 00:50:15 -0600

    working pfticker actually selected

--- pfticker.py
+++ pfticker.py
@@ -2,7 +2,15 @@
 #basis: https://www.geeksforgeeks.org/build-an-application-to-extract-news-from-google-news-feed-using-python/
   
 import sys
-
+import time
+import requests
+from datetime import datetime, timedelta
+# Set the time for the RSS reader later.
+# We don't want to poll RSS more than twice an hour
+time_reset = 0
+later = datetime.now() + timedelta(minutes=31)
+#wonky shit because you can't compare two datetimes without stripping the details
+rss_reset_time = later.strftime("%X")
 
 def check_internet():
     #Validate we are online, since everything is internet-based.
@@ -15,30 +23,8 @@ def check_internet():
         print("You're offline. Exiting")
         quit()
 check_internet()
-
-
-    
-TTOPICS = ['technology', 'business', 'world', 'nation', 'sport']
-CWHITE =  '\33[37m'
-CCYAN = '\33[36m'
-CGREEN = '\33[32m'
-CEND = '\33[0m'
-CWHITE2 = '\33[97m'
-CBLUE2 = '\33[94m'
-CGREY    = '\33[90m'
-CYELLOW2 = '\33[93m'
-
-VAR_RSS = ['https://www.aljazeera.com/xml/rss/all.xml',
-               'https://hnrss.org/frontpage',       
-               'https://www.zdnet.com/news/rss.xml']
-TOTAL_RSS = 3
-TOTAL_ENTRIES = TOTAL_RSS * 4
-NOW = datetime.now()
-LATER = datetime.now() + timedelta(minutes=31)
-RSS_RESET = 0
-FEED_ARRAY = []
-
-
+ 
+#Start of the main loop
 TICKER=1
 while (TICKER != 0):    #because why not?  runs until killed
 
@@ -70,54 +56,88 @@ while (TICKER != 0):    #because why not?  runs until killed
                 time.sleep(4)
     read_google_news()
 
-#generates txt weather report.  Uses OS-defined location
-    VAR_URL="http://wttr.in/stl?2n"
-    import requests
-    VAR_RES = requests.get(VAR_URL)
-    print(VAR_RES.text)
+#generates txt weather report.
+    wttr_url="http://wttr.in/stl?2n"
+    var_res = requests.get(wttr_url)
+    print(var_res.text)
     time.sleep(10)
 
 #Start of the RSS Feed
 #We only want to poll RSS Feeds twice per hour
-    if RSS_RESET == 0:                                     
-        RSS_CNTR = 0                                               
-        ARRAY_CNT = 0                                      
-        ENTRY_LOOP = 0
-        print(CGREEN + "Refreshing RSS feeds" + CEND)
-                                          
-        while RSS_CNTR < TOTAL_RSS:
-            RSSENTRY = feedparser.parse(VAR_RSS[RSS_CNTR])
-            E_CNT = len(RSSENTRY['entries'])
-            if E_CNT > 4:
-                E_CNT = 4                    
-            while ENTRY_LOOP < E_CNT:                     
-                FEEDENTRY = RSSENTRY.entries[ENTRY_LOOP]
-                if FEEDENTRY == None:
-                    pass
-                else:
-                    FEED_ARRAY.append(FEEDENTRY)
-                    ARRAY_CNT += 1
-                ENTRY_LOOP += 1                                                                                                             
-            ENTRY_LOOP = 0
-            RSS_CNTR += 1                                  
-        RSS_RESET = 1                                       
-    else:
-        print(CGREEN + "Recycling RSS feeds" + CEND)    
-    ENTRY_NO = 0
 
-    while ENTRY_NO < ARRAY_CNT:
-        entry = FEED_ARRAY[ENTRY_NO]
-        print(CWHITE2 + entry.published + CEND)
+def rss_newsreader():
+    #import time
+    from datetime import datetime
+    import feedparser
+    import sys
+
+    rss_feed = ['https://www.aljazeera.com/xml/rss/all.xml',
+           'https://hnrss.org/frontpage',
+           'https://www.zdnet.com/news/rss.xml']
+    total_rss = 3 ##need to make this a variable
+
+    feed_array = []
+    rss_cntr = 0
+    array_cnt = 0
+    entry_loop = 0
+    while rss_cntr < total_rss:
+        rss_entry = feedparser.parse(rss_feed[rss_cntr])
+        e_cnt = len(rss_entry['entries'])
+        if e_cnt > 4:
+            e_cnt = 4
+        while entry_loop < e_cnt:
+            feed_entry = rss_entry.entries[entry_loop]
+            if feed_entry == None:
+                pass
+            else:
+                feed_array.append(feed_entry)
+                array_cnt += 1
+            entry_loop += 1
+        entry_loop = 0
+        rss_cntr += 1
+ 
+    return (feed_array)  
+
+def rss_terminal_output(rss_array):
+    rss_array = rss_newsreader()
+    CGREEN = '\33[32m'
+    CEND = '\33[0m'
+    CWHITE2 = '\33[97m'
+    CGREY = '\33[90m'
+    CYELLOW2 = '\33[93m'
+    CBLUE2 = '\33[94m'
+    import feedparser
+    import time
+    for rss_entry in rss_array:
+        print(CWHITE2 + rss_entry.published + CEND)
         print(CYELLOW2 + "******" + CEND)
-        print(CGREY + entry.summary + CEND)
+        print(CGREY + rss_entry.summary + CEND)
         print(CBLUE2 + "------News Link--------")
-        print(CBLUE2 + entry.link + CEND)
+        print(CBLUE2 + rss_entry.link + CEND)
         time.sleep(5)
-        ENTRY_NO += 1
-
-    if NOW >= LATER: 
-        LATER = datetime.now() + timedelta(minutes=31)
-        RSS_RESET = 0
-    print (CGREEN + "End of Cycle" + CEND)
+    print(CGREEN + "End of Cycle" + CEND)
 
 
+#Print the newsfeeds
+import time
+CGREEN = '\33[32m'
+CEND = '\33[0m'
+var_rss = ['https://www.aljazeera.com/xml/rss/all.xml',
+           'https://hnrss.org/frontpage',
+           'https://www.zdnet.com/news/rss.xml']
+
+
+if (time_reset == 0):
+    rss_newsreader()
+    time_reset = 1
+    later = datetime.now() + timedelta(minutes=31)
+    rss_reset_time = later.strftime("%X")
+    print(CGREEN + "Refreshing RSS feeds" + CEND)
+
+rss_array = rss_newsreader()
+
+rss_terminal_output(rss_array)
+now_time = datetime.now()
+now = now_time.strftime("%X")
+if now > rss_reset_time:
+    time_reset = 0
