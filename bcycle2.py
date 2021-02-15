11ca07c2a71ff15c1ed2c5ee2cb98e2f5d7d4f4d
Pat Findley <pat@findley.cc>
2021-02-15 15:38:43 -0600

    comment out youtube loop, add dns5

--- bcycle2.py
+++ bcycle2.py
@@ -4,10 +4,13 @@
 # browser.link.open_newwindow.restriction = 0
 # browser.link.open_newwindow = 1
 # Remember to go into options and enable autoplay audio and video if using Youtube links
+    #from selenium import webdriver
+    #driver = webdriver.Chrome()
+    #driver.get(link1)
 
 import webbrowser
 import time
-import random
+#import random
 
 VAR_CYCLE = 1
 while VAR_CYCLE > 0:
@@ -105,6 +108,11 @@ while VAR_CYCLE > 0:
     firefox.open(af_website, new=0)
     time.sleep(120)
 
+    #ao = pihole docker on 780white (add 15 Feb 2021)
+    ao_website = "http://dns5.findley.cc/admin/"
+    firefox.open(ao_website, new=0)
+    time.sleep(120)
+
     #am = stl business journal (requires sign-in, added 10Jan21)
     am_website = "https://www.bizjournals.com/stlouis/news/"
     firefox.open(am_website, new=0)
@@ -136,36 +144,36 @@ while VAR_CYCLE > 0:
 
     # choose a random element from a list
     # seed random number generator
-    random.seed()
+    #random.seed()
     #create a chance to determine if a video plays this cycle
-    chance = random.randint(0, 11)
+    #chance = random.randint(0, 11)
     # prepare a sequence
-    y1_site = ["https://www.youtube.com/embed/SUyDcyHpFhc?mute=1;autoplay=1", 86] #86 seconds flying past galaxies
-    y2_site = ["https://www.youtube.com/embed/6tmbeLTHC_0?start=54&end=174;mute=1;autoplay=1", 120] #120 seconds the sun 1
-    y3_site = ["https://www.youtube.com/embed/6tmbeLTHC_0?start=267&end=387;mute=1;autoplay=1", 120] #120 seconds the sun 2
-    y4_site = ["https://www.youtube.com/embed/6tmbeLTHC_0?start=559&end=679;mute=1;autoplay=1", 120] #120 seconds the sun 3
-    y5_site = ["https://www.youtube.com/embed/6tmbeLTHC_0?start=1059&end=1359;mute=1;autoplay=1", 300] #300 seconds the sun 4
-    y6_site = ["https://www.youtube.com/embed/7fYKMCCPh28?start=53&end=118;mute=1;autoplay=1", 65] #65 seconds earth 1
-    y7_site = ["https://www.youtube.com/embed/7fYKMCCPh28?start=988&end=1043;mute=1;autoplay=1", 55] #55 seconds earth 2
-    y8_site = ["https://www.youtube.com/embed/7fYKMCCPh28?start=1196&end=1826;mute=1;autoplay=1", 630] #630 seconds earth 3
-    y9_site = ["https://www.youtube.com/embed/7fYKMCCPh28?start=1828&end=2175;mute=1;autoplay=1", 347] #347 seconds earth 4
-    y10_site = ["https://www.youtube.com/embed/PBJAR3-UvSQ?start=29&end=289;mute=1;autoplay=1", 260] #260 seconds aurora
-    y11_site = ["https://www.youtube.com/embed/Ilifg26TZrI?mute=1;autoplay=1", 144] #144 seconds around the moon
-    videos = [y1_site, y2_site, y3_site, y4_site, y5_site, y6_site, y7_site, y8_site, y9_site, y10_site, y11_site]
+    #y1_site = ["https://www.youtube.com/embed/SUyDcyHpFhc?mute=1;autoplay=1", 86] #86 seconds flying past galaxies
+    #y2_site = ["https://www.youtube.com/embed/6tmbeLTHC_0?start=54&end=174;mute=1;autoplay=1", 120] #120 seconds the sun 1
+    #y3_site = ["https://www.youtube.com/embed/6tmbeLTHC_0?start=267&end=387;mute=1;autoplay=1", 120] #120 seconds the sun 2
+    #y4_site = ["https://www.youtube.com/embed/6tmbeLTHC_0?start=559&end=679;mute=1;autoplay=1", 120] #120 seconds the sun 3
+    #y5_site = ["https://www.youtube.com/embed/6tmbeLTHC_0?start=1059&end=1359;mute=1;autoplay=1", 300] #300 seconds the sun 4
+    #y6_site = ["https://www.youtube.com/embed/7fYKMCCPh28?start=53&end=118;mute=1;autoplay=1", 65] #65 seconds earth 1
+    #y7_site = ["https://www.youtube.com/embed/7fYKMCCPh28?start=988&end=1043;mute=1;autoplay=1", 55] #55 seconds earth 2
+    #y8_site = ["https://www.youtube.com/embed/7fYKMCCPh28?start=1196&end=1826;mute=1;autoplay=1", 630] #630 seconds earth 3
+    #y9_site = ["https://www.youtube.com/embed/7fYKMCCPh28?start=1828&end=2175;mute=1;autoplay=1", 347] #347 seconds earth 4
+    #y10_site = ["https://www.youtube.com/embed/PBJAR3-UvSQ?start=29&end=289;mute=1;autoplay=1", 260] #260 seconds aurora
+    #y11_site = ["https://www.youtube.com/embed/Ilifg26TZrI?mute=1;autoplay=1", 144] #144 seconds around the moon
+    #videos = [y1_site, y2_site, y3_site, y4_site, y5_site, y6_site, y7_site, y8_site, y9_site, y10_site, y11_site]
     
     # do we feel lucky?
     #modified to remove the link since Firefox broke autoplay
-    if chance >= 11:
-        selection = random.choice(videos)
-        firefox.open(selection[0], new=0)
-        time.sleep(selection[1])
-    else:
+    #if chance >= 11:
+    #    selection = random.choice(videos)
+    #    firefox.open(selection[0], new=0)
+    #    time.sleep(selection[1])
+    #else:
         #r = text weather in STL
-        r_website = "http://wttr.in/Saint+Louis+Missouri"
-        firefox.open(r_website, new=0)
-        time.sleep(60)
+        #r_website = "http://wttr.in/Saint+Louis+Missouri"
+        #firefox.open(r_website, new=0)
+        #time.sleep(60)
 
-        #latest = AN - 10Jan21
+        #latest = AO - 15Feb21
 
     #a = MSN news page
     #a_website = "https://www.msn.com/en-us/news/"
@@ -212,6 +220,3 @@ while VAR_CYCLE > 0:
     #firefox.open(g_website, new=0)
     #time.sleep(60)
 
-    #from selenium import webdriver
-    #driver = webdriver.Chrome()
-    #driver.get(link1)
