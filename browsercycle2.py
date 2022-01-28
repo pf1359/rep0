28060702f05686020476de881aec3f512c47cf41
Pat Findley <pf1359@github.com>
2022-01-27 22:39:10 -0600

    remove rick's cafe

--- browsercycle2.py
+++ browsercycle2.py
@@ -104,7 +104,7 @@ websites = [site01, site02, site03, site38, site05, \
           site16, site17, site18, \
               site41, site23, site24, site25, \
                   site26, site29, site31, site33, site35, site36, site37, \
-                    site42, site50, site48, site52, site53, site56, site59]
+                    site42, site50, site48, site53, site56, site59]
 
 
 
