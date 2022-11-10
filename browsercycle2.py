9e3ead121df141b0af6a4290bd1d4b35703b4cfb
Pat Findley <pat@findley.cc>
2022-11-10 17:25:21 -0600

    remove postseason.  astros are cheaters.

--- browsercycle2.py
+++ browsercycle2.py
@@ -102,7 +102,7 @@ site60 = ["https://my.papertrailapp.com/events", 180] #Papertrail logging
 websites = [site01, site02, site03, site38, site05, \
     site06, site10, \
         site11, site39, site12, site40, site13, site14, \
-          site16, site17, site18, site43, \
+          site16, site17, site18, \
               site41, site23, site24, site25, \
                   site26, site29, site31, site33, site35, site36, site37, \
                     site42, site50, site48, site53, site54, site57, site60]
