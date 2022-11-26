944834167ccf6110728ed5e7bd5724fa0a40ede1
Pat Findley <pat@findley.cc>
2022-11-25 18:15:15 -0600

    added feedreader to the list

--- browsercycle2.py
+++ browsercycle2.py
@@ -103,7 +103,7 @@ site61 = ["https://feedreader.com/online/#/reader/category/all/", 180] #Feedread
 websites = [site01, site02, site03, site38, site05, \
     site06, site10, \
         site11, site39, site12, site40, site13, site14, \
-          site16, site17, site18, \
+          site16, site17, site18, site61,\
               site41, site23, site24, site25, \
                   site26, site29, site31, site33, site35, site36, site37, \
                     site42, site50, site48, site53, site54, site57, site60]
