07b79e24b87de7bae5e64c7d3408ac6e6cf34e16
Pat Findley <p1359@github.com>
2023-08-13 14:49:49 -0500

    first version of pibrowser

---
+++ pibrowser.py
@@ -0,0 +1,52 @@
+# Modified version of my script for cross-platform use.
+# Pat Findley 17 Apr 2021
+# Copied to picycle on 13 Aug 2023 for raspberry pi
+# To get Firefox to open pages in the same tab, you have to make these changes in about:config:
+# browser.link.open_newwindow.restriction = 0
+# browser.link.open_newwindow = 1
+# browser.cache.memory.limit < - set to something realistic or it will eat all your rams.
+# Remember to go into options and enable autoplay audio and video if using Youtube links
+    #from selenium import webdriver
+    #driver = webdriver.Chrome()
+    #driver.get(link1)
+#import random
+
+#All this is commented out and it uses the default browser.
+#Opening in the same tab only works in Firefox (new=0), and only if you make the changes above.
+#firefox_path = "C:\Program Files\Mozilla Firefox\firefox.exe %s"
+#webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(firefox_path))
+#IF Linux, we need this and change webbrowser.open to firefox.open
+#firefox = webbrowser.get('Firefox')
+
+import webbrowser
+import time
+
+#########
+# Determine platform and define Firefox instead of using default
+import platform
+hostplatform = platform.system()
+if hostplatform =='Windows':
+    webbrowser.register('Firefox',
+	   None,
+	   webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
+
+usebrowser = webbrowser.get('Firefox --kiosk')
+
+site01 = ["http://magicmirror.findley.cc:8080", 240]
+site02 = ["https://www.pollen.com/forecast/current/pollen/63132", 30]
+#site04 = Wunderground US Radar
+site04 = ["https://www.wunderground.com/wundermap?lat=37.7&lon=-92.7&zoom=4&radar=1&wxstn=0", 60]
+site05 = ["http://en.blitzortung.org/live_lightning_maps.php?map=30", 60]
+site06 = ["https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOiJ3ZWF0aGVyIiwiY2VudGVyIjpbLTkwLjIsMzguNjI4XSwiem9vbSI6NywibG9jYXRpb24iOlstOTAuMiwzOC42MjhdfSwiYmFzZSI6InN0YW5kYXJkIiwiY291bnR5IjpmYWxzZSwiY3dhIjpmYWxzZSwic3RhdGUiOmZhbHNlLCJtZW51Ijp0cnVlLCJzaG9ydEZ1c2VkT25seSI6ZmFsc2V9#/", 120]
+#site06 = wunderground temp readings
+site07 = ["https://www.wunderground.com/wundermap", 60]
+site08 = ["https://www.wunderground.com/dashboard/pws/KMOSTLOU477", 60]
+
+websites = [site01, site02, site04, site05, site06, site07, site08]
+
+VAR_CYCLE = 1
+while VAR_CYCLE > 0:
+   for website in websites:
+      usebrowser.open(website[0], new=0)
+      time.sleep(website[1])
+
