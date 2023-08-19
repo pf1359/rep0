407906b9f237ee20ef9ab216fb156d6fa3239ad4
Pat Findley <p1359@github.com>
2023-08-18 20:10:44 -0500

    shell edits

--- pibrowser.sh
+++ pibrowser.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-firefox-esr --kiosk http://magicmirror.findley.cc:8080 &
-sleep 60
+firefox-esr --kiosk 'http://magicmirror.findley.cc:8080' &
+sleep 240
 
-firefox-esr --kiosk https://www.pollen.com/forecast/current/pollen/63132 &
-sleep 60
+firefox-esr --kiosk 'https://www.pollen.com/forecast/current/pollen/63132' &
+sleep 30
 kill %1
 
-firefox-esr --kiosk http://en.blitzortung.org/live_lightning_maps.php?map=30 &
-sleep 60
+firefox-esr --kiosk 'http://en.blitzortung.org/live_lightning_maps.php?map=30' &
+sleep 30
 kill %1
 
-firefox-esr --kiosk https://www.wunderground.com/wundermap?lat=37.7&lon=-92.7&zoom=4&radar=1&wxstn=0 &
+firefox-esr --kiosk 'https://www.wunderground.com/wundermap?lat=37.7&lon=-92.7&zoom=4&radar=1&wxstn=0' &
 sleep 60
 kill%1
 
-firefox-esr --kiosk http://magicmirror.findley.cc:8080 &
-sleep 60
+firefox-esr --kiosk 'http://magicmirror.findley.cc:8080' &
+sleep 180
 kill%1
 
-firefox-esr --kiosk https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOiJ3ZWF0aGVyIiwiY2VudGV &
+firefox-esr --kiosk 'https://radar.weather.gov/?settings=v1_eyJhZ2VuZGEiOnsiaWQiOiJ3ZWF0aGVyIiwiY2VudGV' &
 sleep 60
 kill %1
 
-firefox-esr --kiosk https://www.wunderground.com/wundermap &
+firefox-esr --kiosk 'https://www.wunderground.com/wundermap' &
 sleep 60
 kill %1
 
-firefox-esr --kiosk https://www.wunderground.com/dashboard/pws/KMOSTLOU477 &
+firefox-esr --kiosk 'https://www.wunderground.com/dashboard/pws/KMOSTLOU477' &
 sleep 60
 kill %1
