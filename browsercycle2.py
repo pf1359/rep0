b586fbdbb5011070832ef0e40ed31418c1860857
Pat Findley <p1359@github.com>
2024-01-05 20:45:55 -0600

    convert to linux

--- browsercycle2.py
+++ browsercycle2.py
@@ -15,7 +15,7 @@
 #firefox_path = "C:\Program Files\Mozilla Firefox\firefox.exe %s"
 #webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(firefox_path))
 #IF Linux, we need this and change webbrowser.open to firefox.open
-#firefox = webbrowser.get('Firefox')
+firefox = webbrowser.get('Firefox')
 
 import webbrowser
 import time
@@ -29,7 +29,7 @@ if hostplatform =='Windows':
 	   None,
 	   webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
 usebrowser = webbrowser.get('Firefox')
-usebrowser = webbrowser.get('firefox') #for Linux
+
 
 site01 = ["https://npr.org", 180]
 site02 = ["https://www.kmov.com/news/", 180]
@@ -123,5 +123,6 @@ websites = [site68, site01, site02, site03, site38, site05, \
 VAR_CYCLE = 1
 while VAR_CYCLE > 0:
    for website in websites:
-      usebrowser.open(website[0], new=0)
+#      usebrowser.open(website[0], new=0) #windows
+      firefox.open(website[0], new=0) #linux
       time.sleep(website[1])
