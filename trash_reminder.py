167567207d0229134832cc00dfc50139698685a0
Pat Findley <pat@findley.cc>
2024-05-05 13:43:43 -0500

    convert trash reminder output to html template

--- trash_reminder.py
+++ trash_reminder.py
@@ -15,7 +15,8 @@ def home():
     # If the week number is even, select the first kid, otherwise select the second
     kid = kids[week_number % 2]
     
-    return f"{kid} needs to check the trash and recycling today."
+    #return f"{kid} needs to check the trash and recycling today."
+    return render_template('trash_template.html', kid=kid)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
