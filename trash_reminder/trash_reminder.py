5d3fb501ca690b6c27b5302ff92c34ece2850c26
Pat Findley <pf1359@github.com>
2025-08-16 12:03:51 -0500

    add Monday notice for trash day

--- trash_reminder/trash_reminder.py
+++ trash_reminder/trash_reminder.py
@@ -12,12 +12,24 @@ kids = ["Adrianna", "Travis"]
 def home():
     # Get the current week number
     week_number = datetime.date.today().isocalendar()[1]
+
+## Added 2025-0816
+    # get the day of the week
+    day_of_week = datetime.date.today().isocalendar()[2]
     
     # If the week number is even, select the first kid, otherwise select the second
     kid = kids[week_number % 2]
     
-    #return f"{kid} needs to check the trash and recycling today."
-    return render_template('trash_template.html', kid=kid)
+    # If the day of the week is not Monday, return a message indicating that it's not trash day
+    if day_of_week != 1:  # 1 corresponds to Monday
+
+        #return f"{kid} needs to check the trash and recycling today."
+        return render_template('trash_template.html', kid=kid)
+    else:
+        # If it is Monday, return a message indicating that it's trash day
+        return render_template('trash_template_Monday.html', kid=kid, trash_day=True)
+    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
